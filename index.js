aff95e49fb8052a2d6a4e8cd6259f14d8fc3bc2c
Rajendra Prajapat <rajandra1999@gmail.com>
2021-10-15 23:56:48 +0530

    Added swagger api documentation support

--- index.js
+++ index.js
@@ -1,68 +1,64 @@
-const express      = require('express');
-const bodyParser   = require('body-parser');
-const mongoose     = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors         = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const expressSession = require("express-session");
 const morgan = require("morgan");
 
-const userRoutes = require("./controllers/userControllers/userRoutes");
-const blogRoutes = require("./controllers/blogControllers/blogRoutes");
-const commentRoutes = require("./controllers/commentControllers/commentRoutes");
+const endpoints = require("./endpoints");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
 
 const app = express();
-require('dotenv').config();   // include configurations 
+require("dotenv").config(); // include configurations
 const PORT = process.env.PORT || 8081;
-
 
 // to allow external urls
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
 });
+
 app.use(morgan("dev"));
 // to allow sessions
-app.use(expressSession({
-    secret:process.env.KEY,
-    store: new expressSession.MemoryStore(), 
-    maxAge: 2 * 60 * 60 * 1000, // 2 hr
-    resave: true,
-    //secure: true, only works with https https://github.com/expressjs/session#options
-    saveUninitialized: true
-}));
+app.use(
+	expressSession({
+		secret: process.env.KEY,
+		store: new expressSession.MemoryStore(),
+		maxAge: 2 * 60 * 60 * 1000, // 2 hr
+		resave: true,
+		//secure: true, only works with https https://github.com/expressjs/session#options
+		saveUninitialized: true,
+	})
+);
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-  
+
 // Make connection with the MongoDB atlas
-mongoose.Promise = global.Promise
-mongoose.connect(process.env.Mongo_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify:false
-}).then(()=>{
-    console.log("Connected to atlas!");
-}).catch((err)=>{
-    console.log("Index.js: line 20: ");
-    console.log(err);
-})
+mongoose.Promise = global.Promise;
+mongoose
+	.connect(process.env.Mongo_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log("Connected to atlas!");
+	})
+	.catch((err) => {
+		console.log("Index.js: line 20: ");
+		console.log(err);
+	});
 
-
-
-//Routes 
-app.get('/',(req,res)=>{
-    res.send('<h1>Hello Duniya!</h1>');
-})
-
-app.use('/api/users/',userRoutes);
-app.use('/api/blogs/',blogRoutes);
-app.use('/api/comments/',commentRoutes);
+app.use(endpoints);
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server running at http://127.0.0.1:${PORT}/`);
+	console.log(`Server running at http://127.0.0.1:${PORT}/`);
 });
-  
